338e1dc8953387c769c1bfa3823232affe4dc7d4
sreenath1212 <ssreenathhpd@gmail.com>
2025-05-04 14:01:29 +0530

    Update fastbot.py

--- fastbot.py
+++ fastbot.py
@@ -4,7 +4,7 @@ import multiprocessing
 from multiprocessing import Pool
 import streamlit as st
 import faiss
-from sentence\_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer
 import numpy as np
 import requests
 import os
